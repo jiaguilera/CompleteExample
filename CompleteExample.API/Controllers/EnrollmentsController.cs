f29d8a2ba10027dbbbb82b6723a4d32f84e4f8ce
José Ignacio Aguilera <jiaguilera@gmail.com>
2022-04-30 16:43:01 -0300

    add EnrollmentController

---
+++ CompleteExample.API/Controllers/EnrollmentsController.cs
@@ -0,0 +1,29 @@
+﻿using CompleteExample.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace CompleteExample.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        [HttpGet("instructor/{instructorId}")]
+        public async Task<IEnumerable<EnrollmentDTO>> ByInstructor([FromServices] IEnrollmentQueries queries, int instructorId)
+        {
+            var results = await queries.ByInstructor(instructorId);
+
+            return results.Select(EnrollmentDTO.FromEntity);
+        }
+
+        [HttpGet("topstudents")]
+        public async Task<IEnumerable<EnrollmentDTO>> ByInstructor([FromServices] IEnrollmentQueries queries)
+        {
+            var results = await queries.TopStudents();
+
+            return results.Select(EnrollmentDTO.FromEntity);
+        }
+    }
+}
---
+++ CompleteExample.API/DTOs/EnrollmentDTO.cs
@@ -0,0 +1,16 @@
+﻿using CompleteExample.Entities;
+
+namespace CompleteExample.API.Controllers
+{
+    public class EnrollmentDTO
+    {
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public decimal Grade { get; set; }
+
+        public static EnrollmentDTO FromEntity(Enrollment entity)
+            => entity != default
+                ? new EnrollmentDTO { StudentId = entity.StudentId, CourseId = entity.CourseId, Grade = entity.Grade }
+                : default;
+    }
+}
